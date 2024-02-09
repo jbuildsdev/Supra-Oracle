0b6d80a49cdaff082afd60ee77b022243c53ad0f
J Builds <jbuildsdev@outlook.com>
2024-02-09 10:12:20 -0600

    Fix error in Supra code example

---
+++ contracts/Oracle.sol
@@ -0,0 +1,55 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.20;
+import "./ISupraSValueFeed.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+
+contract Oracle is  Ownable {
+
+ISupraSValueFeed internal sValueFeed;
+
+
+constructor(ISupraSValueFeed _sValueFeed) Ownable(msg.sender){
+sValueFeed=_sValueFeed;
+
+}
+
+
+function updateSupraSvalueFeed(ISupraSValueFeed _newSValueFeed)
+external
+onlyOwner
+ {
+
+
+sValueFeed = _newSValueFeed;
+}
+
+
+function getSupraSvalueFeed() external view returns(ISupraSValueFeed){
+return sValueFeed;
+}
+
+
+function getPrice(uint256 _priceIndex)
+external
+view
+returns (ISupraSValueFeed.priceFeed memory) {
+return sValueFeed.getSvalue(_priceIndex);
+}
+
+
+function getPriceForMultiplePair(uint256[] memory _pairIndexes)
+external
+view
+returns (ISupraSValueFeed.priceFeed[] memory) {
+return sValueFeed.getSvalues(_pairIndexes);
+}
+
+function getDerivedValueOfPair(uint256 pair_id_1,uint256 pair_id_2,uint256 operation)
+external
+view
+returns(ISupraSValueFeed.derivedData memory){
+return sValueFeed.getDerivedSvalue(pair_id_1,pair_id_2,operation);
+}
+}
